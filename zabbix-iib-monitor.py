392458b8149148f844a672938489efb15937bd9f
Jokinen Peter <peter.jokinen@digia.com>
2018-04-26 08:59:37 +0300

    config file test

--- zabbix-iib-monitor.py
+++ zabbix-iib-monitor.py
@@ -42,35 +42,6 @@ TOPICS= [
    ("IBM/IntegrationBus/" + integrationNodeName + "/Status/ExecutionGroup/" + integrationServerName, 0) ,
    ("IBM/IntegrationBus/" + integrationNodeName + "/Statistics/JSON/SnapShot/" + integrationServerName + "/applications/" + applicationName + "/messageflows/" + messageFlowName, 0)
    
-]
-'''
-
-LOG TIME FORMATING
-
-%a 	Locale's abbreviated weekday name. 	 
-%A 	Locale's full weekday name. 	 
-%b 	Locale's abbreviated month name. 	 
-%B 	Locale's full month name. 	 
-%c 	Locale's appropriate date and time representation. 	 
-%d 	Day of the month as a decimal number [01,31]. 	 
-%H 	Hour (24-hour clock) as a decimal number [00,23]. 	 
-%I 	Hour (12-hour clock) as a decimal number [01,12]. 	 
-%j 	Day of the year as a decimal number [001,366]. 	 
-%m 	Month as a decimal number [01,12]. 	 
-%M 	Minute as a decimal number [00,59]. 	 
-%p 	Locale's equivalent of either AM or PM.
-%S 	Second as a decimal number [00,61].
-%U 	Week number of the year (Sunday as the first day of the week) as a decimal number [00,53]. All days in a new year preceding the first Sunday are considered to be in week 0. 	(3)
-%w 	Weekday as a decimal number [0(Sunday),6]. 	 
-%W 	Week number of the year (Monday as the first day of the week) as a decimal number [00,53]. All days in a new year preceding the first Monday are considered to be in week 0. 	(3)
-%x 	Locale's appropriate date representation. 	 
-%X 	Locale's appropriate time representation. 	 
-%y 	Year without century as a decimal number [00,99]. 	 
-%Y 	Year with century as a decimal number. 	 
-%Z 	Time zone name (no characters if no time zone exists). 	 
-%% 	A literal '%' character. 	 
-'''
-datetimeFormat = ConfigSectionMap("LOGGING")['DatetimeFormat']
 
 ##### CODE #####
 
@@ -194,6 +165,7 @@ def on_log(client, userdata, level, buf):
    f.close()
 
 def get_timeStr():
+   datetimeFormat = ConfigSectionMap("LOGGING")['DatetimeFormat']
    ts = time.localtime()
    timeStr = time.strftime(datetimeFormat , ts)
    return timeStr
