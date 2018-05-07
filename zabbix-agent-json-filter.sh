8ae4561969e93f8ab8da7de19f16b94dca86c222
Jokinen Peter <peter.jokinen@digia.com>
2018-05-07 11:58:31 +0300

    zabbix agent script test

--- zabbix-agent-json-filter.sh
+++ zabbix-agent-json-filter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 
-cat zabbix-iib-monitor-agent-data.json | jq 'if . == IBM/IntegrationBus/IB10NODE/Status then {"test":"started", "test2":"stopped"}'
+cat zabbix-iib-monitor-agent-data.json | jq 'if . == IBM/IntegrationBus/IB10NODE/Status then {"test":"started", "test2":"stopped"} end'
