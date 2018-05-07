35c19b75eac1f2c828bdb1df463dc93044b66257
Jokinen Peter <peter.jokinen@digia.com>
2018-05-07 13:57:37 +0300

    zabbix agent script test

--- zabbix-agent-json-filter.sh
+++ zabbix-agent-json-filter.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 #
 
-cat zabbix-iib-monitor-agent-data.json | jq 'if . == "IBM/IntegrationBus/IB10NODE/Status" then {"test":"started", "test2":"stopped"} else . end'
+cat zabbix-iib-monitor-agent-data.json | jq '
+{
+   "{NODE}":."IBM/IntegrationBus/IB10NODE/Statistics/JSON/SnapShot/server1/applications/IoTonCloud/messageflows/TemperatureSensor".WMQIStatisticsAccounting.MessageFlow.BrokerLabel, 
+   "{SERVER}":."IBM/IntegrationBus/IB10NODE/Statistics/JSON/SnapShot/server1/applications/IoTonCloud/messageflows/TemperatureSensor".WMQIStatisticsAccounting.MessageFlow.ExecutionGroupName, 
+   "{APP}":."IBM/IntegrationBus/IB10NODE/Statistics/JSON/SnapShot/server1/applications/IoTonCloud/messageflows/TemperatureSensor".WMQIStatisticsAccounting.MessageFlow.ApplicationName, 
+   "{MSGFLOW}":.  "IBM/IntegrationBus/IB10NODE/Statistics/JSON/SnapShot/server1/applications/IoTonCloud/messageflows/TemperatureSensor".WMQIStatisticsAccounting.MessageFlow.MessageFlowName
+}
+'
