01c33f85fcceecb3e549471a2dacd7ce7e01e703
Jokinen Peter <peter.jokinen@digia.com>
2018-05-14 12:13:02 +0300

    zabbix script improvements

--- etc/zabbix/scripts/iib.messageflow.sh
+++ etc/zabbix/scripts/iib.messageflow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 
-cat /tmp/zabbix-iib-monitor-agent-data.json | jq --arg TOPIC "$1" --arg DATA "$2" '.[$TOPIC].WMQIStatisticsAccounting.MessageFlow.[$DATA]'
+cat /tmp/zabbix-iib-monitor-agent-data.json | jq --arg TOPIC "$1" --arg DATA "$2" ' if [$DATA] != null OR [$DATA] != "" then .[$TOPIC].WMQIStatisticsAccounting.MessageFlow[$DATA] else .[$TOPIC].WMQIStatisticsAccounting.MessageFlow end'
--- etc/zabbix/scripts/iib.mqtt_topic.discovery.sh
+++ etc/zabbix/scripts/iib.mqtt_topic.discovery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 
-cat /tmp/zabbix-iib-monitor-agent-data.json | jq '{ "data": [keys[] | foreach . as $item (.; if $item | test("IBM/IntegrationBus/.*/Status$") then {"{#TOPIC_NODE}":$item} elif $item | test("^IBM/IntegrationBus/.*/Statistics/JSON/SnapShot/") then {"{#TOPIC_MESSAGEFLOW}":$item} elif $item | test("^IBM/IntegrationBus/.*/Status/ExecutionGroup/.*$") then {"{#TOPIC_SERVER}":$item} else . end)]}'
+cat /tmp/zabbix-iib-monitor-agent-data.json | jq '{ "data": [keys[] | foreach . as $item (.; if $item | test("IBM/IntegrationBus/.*/Status$") then {"{#TOPIC_NODE}":$item} elif $item | test("^IBM/IntegrationBus/.*/Statistics/JSON/SnapShot/") then {"{#TOPIC_MESSAGEFLOW}":$item} elif $item | test("^IBM/IntegrationBus/.*/Status/ExecutionGroup/.*$") then {"{#TOPIC_SERVER}":$item} else {"Error unknown topic":"$item"} end)]}'
