d977e67db81a5c7207a39ed4bd3532bfc19dad21
Jokinen Peter <peter.jokinen@digia.com>
2018-05-16 14:35:36 +0300

    zabbix script improvements

--- etc/zabbix/scripts/iib.mqtt_topic.discovery.sh
+++ etc/zabbix/scripts/iib.mqtt_topic.discovery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-cat /tmp/zabbix-iib-monitor-agent-data.json | jq -S --arg TOPIC_TYPE '{ "data": [keys[] | foreach . as $item (.;  
+cat /tmp/zabbix-iib-monitor-agent-data.json | jq -S --arg TOPIC_TYPE "$1" '{ "data": [keys[] | foreach . as $item (.;  
 if $TOPIC_TYPE == "node" then (if $item | test("IBM/IntegrationBus/.*/Status$") then {"{#TOPIC_NODE}":$item} else empty end)  
 elif $TOPIC_TYPE== "server" then (if $item | test("^IBM/IntegrationBus/.*/Status/ExecutionGroup/.*$") then {"{#TOPIC_SERVER}":$item} else empty end)  
 elif $TOPIC_TYPE== "messageflow" then (if $item | test("^IBM/IntegrationBus/.*/Statistics/JSON/SnapShot/") then {"{#TOPIC_MESSAGEFLOW}":$item} else empty end) 
