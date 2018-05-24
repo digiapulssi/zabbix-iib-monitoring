f8029b216bac8f87df422b6f26faf9ff2811eeca
Jokinen Peter <peter.jokinen@digia.com>
2018-05-24 13:22:04 +0300

    MQTT threading test

--- zabbix-iib-monitor.py
+++ zabbix-iib-monitor.py
@@ -7,6 +7,7 @@ import re
 from xmljson import abdera as ab
 from xml.etree.ElementTree import fromstring
 import ConfigParser
+import logging
 
 ##### CONFIG #####
 # path to config file
@@ -48,138 +49,70 @@ def ConfigSectionMap(section):
         try:
             dict1[option] = config.get(section, option)
             if dict1[option] == -1:
-               f=open(logFileName, "a+")
-               f.write(get_timeStr() + " Configuration file, skip: %s" % option)
-               f.close()
+               logging.info("Configuration file, skip: %s" % option)
         except:
-            f=open(logFileName, "a+")
-            f.write(get_timeStr() + " Exception with configuration file on %s!" % option)
-            f.close()
+            logging.info("Exception with configuration file on %s!" % option)
             dict1[option] = None
     return dict1
 
 def on_message(client, userdata, message):
-   f=open(logFileName, "a+")
 
    if printMsg:
-      f.write(get_timeStr() + " Message Recieved: " + str(message.payload.decode("utf-8") + "\n"))
-      f.write(get_timeStr() + " Message topic=" + message.topic + "\n")
-      f.write(get_timeStr() + " Message qos=" + str(message.qos) + "\n")
-      f.write(get_timeStr() + " Message retain flag=" + str(message.retain) + "\n")
+      logging.info("Message Recieved: " + str(message.payload.decode("utf-8")))
+      logging.info("Message topic=" + message.topic)
+      logging.info("Message qos=" + str(message.qos))
+      logging.info("Message retain flag=" + str(message.retain))
    else:
-      f.write(get_timeStr() +  " Message from topic: " + message.topic + "\n")
-   f.close()
+      logging.info("Message from topic: " + message.topic)
    
    pattern = "(IBM/IntegrationBus/(\w+)/Status/ExecutionGroup/(\w+))|(IBM/IntegrationBus/(\w+)/Status)"
    match = re.match(pattern, message.topic)
    
    obj = {}
    if match == None:
-      if os.path.isfile(jsonFileName):
+      if os.path.isfile(jsonFile):
          try:
-            with open(jsonFileName, 'r') as jsonFile:
-               obj = json.load(jsonFile)
+            jsonStr = f.read()
+            obj = json.loads(jsonStr)
                
             obj[str(message.topic)] = json.loads(message.payload.decode("utf-8"))
                   
-            with open(jsonFileName, 'w') as outfile:
+            with open(jsonFile, 'w') as outfile:
                json.dump(obj, outfile)
-               f=open(logFileName, "a+")
-               f.write(get_timeStr() + " Write Complete\n")
-               f.close()
+               logging.info("Write Complete")
             
          except ValueError: 
-            f=open(logFileName, "a+")
-            f.write(get_timeStr() + " Error while reading JSON\n")
-            f.close()
+            logging.error("Error while reading JSON")
       
       else:
          try:
             jsonObj = {}
             jsonObj[message.topic] = str(message.payload.decode("utf-8"))
-            with open(jsonFileName, 'w') as outfile:
+            with open(jsonFile, 'w') as outfile:
                json.dump(jsonObj, outfile)
-               f=open(logFileName, "a+")
-               f.write(get_timeStr() + " Write Complete\n")
-               f.close()
+               logging.info("Write Complete\n")
                
          except ValueError: 
-            f=open(logFileName, "a+")
-            f.write(get_timeStr() + " Error while writing to JSON file\n")
-            f.close()
+            logging.error("Error while writing to JSON file")
    else:
       parsedJSON = ab.data(fromstring(str(message.payload.decode("utf-8"))))
       
       try:
-         if os.path.isfile(jsonFileName):
-            with open(jsonFileName, 'r') as jsonFile:
-               obj = json.load(jsonFile)
+         if os.path.isfile(jsonFile):
+            jsonStr = f.read()
+            obj = json.loads(jsonStr)
                
          obj[str(message.topic)] = parsedJSON
          
-         with open(jsonFileName, 'w') as outfile:
+         with open(jsonFile, 'w') as outfile:
             json.dump(obj, outfile)
             
-            f=open(logFileName, "a+")
-            f.write(get_timeStr() + " Write Complete\n")
-            f.close()
+            logging.info("Write Complete")
          
       except ValueError: 
-         f=open(logFileName, "a+")
-         f.write(get_timeStr() + " Error while reading JSON\n")
-         f.close()
+         logging.error("Error while reading JSON")
 
 def on_connect(client, userdata, flags, rc):
-   f=open(logFileName, "a+")
-   f.write(get_timeStr() + " " + conn_codes[rc] + ". (code " + str(rc) + ")\n")
-   f.close()
-   client.subscribe(TOPICS)
-
-def on_subscribe(client, userdata, mid, granted_qos):
-   f=open(logFileName, "a+")
-   f.write(get_timeStr() + " Successfuly subscribed to topic(s)\n")
-   f.close()
-
-def on_unsubscribe(client, userdata, mid):
-   f=open(logFileName, "a+")
-   f.write(get_timeStr() + " Unsubscribed from " + TOPIC + "\n")
-   f.close()
-
-def on_disconnect(client, userdata, rc):
-   f=open(logFileName, "a+")
-
-   if rc != 0:
-      f.write(get_timeStr() + " Unexpected disconnection.\n")
-   else:
-      f.write(get_timeStr() + " Disconnected\n")
-   f.close()
-
-def on_log(client, userdata, level, buf):
-   f=open(logFileName, "a+")
-   f.write(get_timeStr() + " Log message: " + str(client) + " " + str(userdata) + " " + str(buf)  + "\n")
-   f.close()
-
-def get_timeStr():
-   datetimeFormat = ConfigSectionMap("LOGGING")['datetimeformat']
-   ts = time.localtime()
-   timeStr = time.strftime(datetimeFormat , ts)
-   return timeStr
-
-if __name__ == "__main__":
-   logPath = ConfigSectionMap("LOGGING")['logpath']
-   JsonFilePath = ConfigSectionMap("GENERAL")['jsonfilepath']
-   
-   enableLogMsg = config.getboolean("LOGGING", "enablelogmsg")
-   printMsg = config.getboolean("LOGGING", "printmsg")
-   
-   BROKER_ADDRESS = ConfigSectionMap("GENERAL")['ip']
-   PORT = ConfigSectionMap("GENERAL")['port']
-   clientId = ConfigSectionMap("GENERAL")['clientid']
-   
-   filename = os.path.splitext(__file__)[0]
-   jsonFileName = JsonFilePath + filename + "-agent-data.json"
-   logFileName = logPath + filename + ".log"
-   
    conn_codes = [
       "Connection successful",
       "Connection refused - incorrect protocol version",
@@ -188,9 +121,84 @@ if __name__ == "__main__":
       "Connection refused - bad username or password",
       "Connection refused - not authorised"
    ]
-      
-   f=open(logFileName, "a+")   
-   f.write(get_timeStr() + " Creating new instance.\n")
+   
+   logging.info(conn_codes[rc] + ". (code " + str(rc) + ")")
+   client.subscribe(TOPICS)
+
+def on_subscribe(client, userdata, mid, granted_qos):
+   logging.info("Successfuly subscribed to topic(s)")
+
+def on_unsubscribe(client, userdata, mid):
+   logging.info("Unsubscribed from topic")
+
+def on_disconnect(client, userdata, rc):
+
+   if rc != 0:
+      logging.error("Unexpected disconnection.")
+   else:
+      logging.info("Disconnected")
+
+def on_log(client, userdata, level, buf):
+   logging.info("Log message: " + str(client) + " " + str(userdata) + " " + str(buf))
+   #f=open(logFile, "a+")
+   #logging.info(get_timeStr() + " Log message: " + str(client) + " " + str(userdata) + " " + str(buf)  + "\n")
+   #f.close()
+
+def get_timeStr():
+   datetimeFormat = ConfigSectionMap("CONFIG")['datetimeformat']
+   ts = time.localtime()
+   timeStr = time.strftime(datetimeFormat , ts)
+   return timeStr
+   
+def thread_MQTT(BROKER_ADDRESS,PORT,id):
+   client = mqtt.Client(id) 
+   
+   client.on_connect = on_connect
+   client.on_message = on_message
+   client.on_subscribe = on_subscribe
+   client.on_unsubscribe = on_unsubscribe
+   client.on_disconnect = on_disconnect
+   
+   if enableLogMsg:
+      client.on_log = on_log
+   
+   logging.info(threading.currentThread().getName() + " Connecting to broker: " + BROKER_ADDRESS + ":" + PORT + " with id: " + id + "\n")
+   client.connect( BROKER_ADDRESS, int(PORT))
+   
+   client.loop_forever()
+
+if __name__ == "__main__":
+   logFile = ConfigSectionMap("CONFIG")['logfile']
+   jsonFile = ConfigSectionMap("CONFIG")['jsonFile']
+   
+   enableLogMsg = config.getboolean("CONFIG", "enablelogmsg")
+   printMsg = config.getboolean("CONFIG", "printmsg")
+   
+   broker_list = ConfigSectionMap("CONFIG")['brokers']
+   
+   #BROKER_ADDRESS = ConfigSectionMap("CONFIG")['ip']
+   #PORT = ConfigSectionMap("CONFIG")['port']
+   #clientId = ConfigSectionMap("CONFIG")['clientid']
+   
+   #filename = os.path.splitext(__file__)[0]
+   
+   f=open(jsonFile, "r+")
+   logging.basicConfig(filename=logFile, filemode='a',format='%(asctime)s  %(levelname)s: %(message)s')
+   brokers=f.readlines()
+   
+   threads = []
+   for i in range (len(open(broker_list).readlines())):
+      b=brokers[i].split(",")
+      t = threading.Thread(target = thread_MQTT, args = (b[0],b[1],b[2]))
+      threads.append(t)
+      t.start()
+   
+   while threading.activeCount() > 1:
+      pass
+   else:
+      f.close()
+   
+   '''
    client = mqtt.Client(clientId) 
    
    client.on_connect = on_connect
@@ -202,8 +210,9 @@ if __name__ == "__main__":
    if enableLogMsg:
       client.on_log = on_log
    
-   f.write(get_timeStr() + " Connecting to broker: " + BROKER_ADDRESS + ":" + PORT + "\n")
+   logging.info(get_timeStr() + " Connecting to broker: " + BROKER_ADDRESS + ":" + PORT + "\n")
    f.close()
    client.connect( BROKER_ADDRESS, int(PORT))
    
    client.loop_forever()
+'''
